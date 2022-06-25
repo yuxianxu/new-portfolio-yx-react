4411e57cdb51d09437358700651e0d35fee2cec1
yuxianxu <yuxian.xu@gmail.com>
2022-06-24 22:11:30 -0700

    Add menu icons for mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import { FacebookRounded, GitHub, LinkedIn } from '@mui/icons-material';
+import { Box, Link } from '@mui/material';
+import React from 'react';
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <Box>
+      <Box
+        sx={{ display: 'flex', justifyContent: 'space-evenly', paddingTop: 4 }}
+      >
+        <Link href="https://linkedin.com/in/yuxianxu">
+          <LinkedIn color="action" sx={{ fontSize: 30 }} />
+        </Link>
+        <Link href="https://github.com/yuxianxu">
+          <GitHub color="action" sx={{ fontSize: 30 }} />
+        </Link>
+        <Link href="https://facebook.com">
+          <FacebookRounded color="action" sx={{ fontSize: 30 }} />
+        </Link>
+      </Box>
+    </Box>
+  );
 }
 
-export default Footer
+export default Footer;
