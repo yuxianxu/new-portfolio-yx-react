c92563e6fdea596e9e3470f8930ee0a28aba9e17
yuxianxu <yuxian.xu@gmail.com>
2022-06-26 17:15:09 -0700

    Add Post ccard

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,14 +1,33 @@
 import { Box } from '@mui/system';
 import React from 'react';
-import BlogPost from '../components/BlogPost';
+import PostCard from '../components/PostCard';
 
 function Blog() {
   return (
-    <Box sx={{ height: '60vh', paddingTop: 10 }}>
-      <BlogPost />
-      <BlogPost />
-      <BlogPost />
-      <BlogPost />
+    <Box
+      sx={{
+        paddingTop: 15,
+      }}
+    >
+      <Box>
+        <Box sx={{ textAlign: 'center', paddingBottom:'44px' }}><h2>My Blogs</h2></Box>
+      </Box>
+      <Box
+        sx={{
+          paddingBottom: 15,
+          maxWidth: '1200px',
+          display: 'flex',
+          justifyContent: 'center',
+          gap: 3,
+          flexWrap: 'wrap',
+          margin: '0 auto',
+        }}
+      >
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+      </Box>
     </Box>
   );
 }
